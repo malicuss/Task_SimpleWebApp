bc94fabc01fd31ca95c4c770d87631ea101111e3
Konstantin Emanov <konstantin_emanov@epam.com>
2022-10-18 16:40:36 +0300

    Moved category logic to separate controller

---
+++ SimpleWebApp/Controllers/CategoriesController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SimpleWebApp.Models;
+
+namespace SimpleWebApp.Controllers;
+
+public class CategoriesController : Controller
+{
+    private readonly NorthwindContext _context;
+
+    public CategoriesController(NorthwindContext context)
+    {
+        _context = context;
+    }
+
+    public IActionResult List()
+    {
+        return View(_context.Categories);
+    }
+}
--- SimpleWebApp/Controllers/HomeController.cs
+++ SimpleWebApp/Controllers/HomeController.cs
@@ -8,24 +8,15 @@ namespace SimpleWebApp.Controllers;
 public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
-    private readonly NorthwindContext _context;
-    private readonly int _productsToShow;
 
     public HomeController(
-        ILogger<HomeController> logger, 
-        NorthwindContext context,
-        IConfiguration configuration)
+        ILogger<HomeController> logger)
     {
         _logger = logger;
-        _context = context;
-        _productsToShow = configuration.GetValue<int>("ProductsToShow");
     }
 
     public IActionResult Index()
         => View();
-
-    public IActionResult Categories()
-        => View(_context.Categories.ToList());
 
     public IActionResult ShowMeException()
     {
